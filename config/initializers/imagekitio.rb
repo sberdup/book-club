85b990d575a7fd5a593af9266541e88f494efca4
Sergio Berdeja <sergioberdeja@gmail.com>
2022-10-16 11:58:50 -0600

    heroku ENV fix

--- config/initializers/imagekitio.rb
+++ config/initializers/imagekitio.rb
@@ -5,9 +5,9 @@ ImageKitIo.configure do |config|
       config.url_endpoint = ENV['IMAGE_KIT_ENDPOINT'] # https://ik.imagekit.io/your_imagekit_id
     end
     if Rails.env.production?
-      config.public_key = process.env.IMAGE_KIT_PUBLIC_API_KEY
-      config.private_key = process.env.IMAGE_KIT_PRIVATE_API_KEY
-      config.url_endpoint = process.env.IMAGE_KIT_ENDPOINT
+      config.public_key = ENV['IMAGE_KIT_PUBLIC_API_KEY']
+      config.private_key = ENV['IMAGE_KIT_PRIVATE_API_KEY']
+      config.url_endpoint = ENV['IMAGE_KIT_ENDPOINT']
     end
     config.service = :carrierwave
     # config.constants.MISSING_PRIVATE_KEY = 'custom error message'
