7519c47b3021acab694344d31c35a0e0de20b3b5
Sergio Berdeja <sergioberdeja@gmail.com>
2022-10-16 20:05:59 -0600

    corrected seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts 'Seeding data...'
+Image.destroy_all
 User.destroy_all
 Book.destroy_all 
 Club.destroy_all
