26e5f6973be89d422423cd7278bf884a91327099
Sergio Berdeja <sergioberdeja@gmail.com>
2022-10-31 21:26:46 -0600

    shifting edit to wikitile

--- client/src/components/WikiTile.jsx
+++ client/src/components/WikiTile.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardBody, CardFooter, CardHeader, Heading, Paragraph, Text } from 'grommet'
+import { Box, Card, CardBody, CardFooter, CardHeader, FormField, TextInput, Paragraph, Text } from 'grommet'
 import WikiDeleteButton from '../subcomponents/WikiDeleteButton'
 import React, { useState } from 'react'
 import WikiEditButton from '../subcomponents/WikiEditButton'
@@ -6,56 +6,43 @@ import WikiEditButton from '../subcomponents/WikiEditButton'
 function WikiTile({ element, category }) {
 
   const [editToggle, setEditToggle] = useState(false)
-
-  function objectToKeys(object) {
-    const elKeys = Object.keys(object)
-    return elKeys
-  }
-  function propertyToJSX(key, element) {
-    if (key === 'id' || element[key] === null || element[key] === '') {
-      return <span style={{ display: 'none' }}></span>
-    }
-    // Not displaying ID or fields with no information
-    switch (key) {
-      case 'chapter':
-      case 'page':
-        return (
-          <h5 style={{ margin: 0 }}>{key.charAt(0).toUpperCase() + key.slice(1) + ': ' + element[key]}</h5>
-        )
-      default:
-        return (
-          <>
-            <h4 style={{ margin: 0 }}>{key.charAt(0).toUpperCase() + key.slice(1)}</h4>
-            <p style={{ marginTop: 0, marginBottom: '10px' }}>{element[key]}</p>
-          </>
-        )
-    }
-  }
-
-  const elementMap = objectToKeys(element).map((key, idx) => {
-    let jsx = propertyToJSX(key, element)
-    return (
-      <div key={idx} style={{ marginRight: '20px', marginLeft: '20px' }}>
-        {jsx}
-      </div>
-    )
+  const [formData, setFormData] = useState({
+    name: element.name, description: element.description, aliases: element.aliases
+    , time: (element.time || ''), location: element.location, chapter: element.chapter, page: element.page, body: element.body
   })
 
+  function inputHandler(e) {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
 
   return (
     <Card width={{ max: 'large' }} height={{ min: 'small' }} background='accent-5'>
       {/* {elementMap} */}
       <CardHeader justify='center' >
-        {element?.name ? <Text margin='small' >{element.name}</Text> : null}
+        {element?.name ?
+          (editToggle ?
+            <FormField label='Name'>
+              <TextInput textAlign='center' type="text" id="name" value={formData.name} onChange={inputHandler}></TextInput>
+            </FormField>
+            : <Text margin='small' >{element.name}</Text>
+          ) : null
+        }
         {element?.body ? <Text margin='medium' >{element.body}</Text> : null}
       </CardHeader>
 
       <CardBody justify='around'>
         {element?.description ? <Text>{element.description}</Text> : null}
         {element?.aliases ? <Text>{element.aliases}</Text> : null}
-        {element?.time ? <Text>{element.time}</Text> : null}
+        {element?.time || (element.time === null) ?
+          (editToggle ?
+            <FormField label='Time'>
+              <input type="datetime-local" id="time" value={formData.time} onChange={inputHandler}></input>
+            </FormField>
+            : <Text>{element.time}</Text>
+          ) : null
+        }
         {element?.location ? <Text>{element.location}</Text> : null}
-        
+
         <Box direction='row' justify='around'>
           {element?.chapter ? <Paragraph size='small' margin='none'>{`Chapter: ${element.chapter}`}</Paragraph> : null}
           {element?.page ? <Paragraph size='small' margin='none'>{`Page: ${element.page}`}</Paragraph> : null}
