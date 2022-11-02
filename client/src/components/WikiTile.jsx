ca4faf1d3dcb0c1791149a31fd11f0e67ee95cf7
Sergio Berdeja <sergioberdeja@gmail.com>
2022-11-01 21:39:47 -0600

    card field editing in sync with state

--- client/src/components/WikiTile.jsx
+++ client/src/components/WikiTile.jsx
@@ -8,7 +8,7 @@ function WikiTile({ element, category }) {
   const [editToggle, setEditToggle] = useState(false)
   const [formData, setFormData] = useState({
     name: element.name, description: element.description, aliases: element.aliases
-    , time: (element.time || ''), location: element.location, chapter: element.chapter, page: element.page, body: element.body
+    , time: element.time, location: element.location, chapter: element.chapter, page: element.page, body: element.body
   })
 
   function inputHandler(e) {
@@ -19,7 +19,7 @@ function WikiTile({ element, category }) {
     <Card width={{ max: 'large' }} height={{ min: 'small' }} background='accent-5'>
       {/* {elementMap} */}
       <CardHeader justify='center' >
-        {element?.name ?
+        {element.name !== undefined ?
           (editToggle ?
             <FormField label='Name'>
               <TextInput textAlign='center' type="text" id="name" value={formData.name} onChange={inputHandler}></TextInput>
@@ -27,13 +27,34 @@ function WikiTile({ element, category }) {
             : <Text margin='small' >{element.name}</Text>
           ) : null
         }
-        {element?.body ? <Text margin='medium' >{element.body}</Text> : null}
+        {element.body !== undefined ?
+          (editToggle ?
+            <FormField label='Body'>
+              <TextInput textAlign='center' type="text" id="body" value={formData.body} onChange={inputHandler}></TextInput>
+            </FormField>
+            : <Text margin='medium' >{element.body}</Text>
+          ) : null
+        }
       </CardHeader>
 
       <CardBody justify='around'>
-        {element?.description ? <Text>{element.description}</Text> : null}
-        {element?.aliases ? <Text>{element.aliases}</Text> : null}
-        {element?.time || (element.time === null) ?
+        {element.description !== undefined ?
+          (editToggle ?
+            <FormField label='Description'>
+              <TextInput textAlign='center' type="text" id="description" value={formData.description} onChange={inputHandler}></TextInput>
+            </FormField>
+            : <Text>{element.description}</Text>
+          ) : null
+        }
+        {element.aliases !== undefined ?
+          (editToggle ?
+            <FormField label='Aliases'>
+              <TextInput textAlign='center' type="text" id="aliases" value={formData.aliases} onChange={inputHandler}></TextInput>
+            </FormField>
+            : <Text>{element.aliases}</Text>
+          ) : null
+        }
+        {element.time !== undefined ?
           (editToggle ?
             <FormField label='Time'>
               <input type="datetime-local" id="time" value={formData.time} onChange={inputHandler}></input>
@@ -41,11 +62,33 @@ function WikiTile({ element, category }) {
             : <Text>{element.time}</Text>
           ) : null
         }
-        {element?.location ? <Text>{element.location}</Text> : null}
+        {element.location !== undefined ?
+          (editToggle ?
+            <FormField label='Location'>
+              <TextInput textAlign='center' type="text" id="location" value={formData.location} onChange={inputHandler}></TextInput>
+            </FormField>
+            : <Text>{element.location}</Text>
+          ) : null
+        }
 
         <Box direction='row' justify='around'>
           {element?.chapter ? <Paragraph size='small' margin='none'>{`Chapter: ${element.chapter}`}</Paragraph> : null}
-          {element?.page ? <Paragraph size='small' margin='none'>{`Page: ${element.page}`}</Paragraph> : null}
+          {element.chapter !== undefined ?
+            (editToggle ?
+              <FormField label='Chapter'>
+                <TextInput textAlign='center' type="number" id="chapter" value={formData.chapter} onChange={inputHandler}></TextInput>
+              </FormField>
+              : <Paragraph size='small' margin='none'>{`Chapter: ${element.chapter}`}</Paragraph>
+            ) : null
+          }
+          {element.page !== undefined ?
+            (editToggle ?
+              <FormField label='Page'>
+                <TextInput textAlign='center' type="number" id="page" value={formData.page} onChange={inputHandler}></TextInput>
+              </FormField>
+              : <Paragraph size='small' margin='none'>{`Page: ${element.page}`}</Paragraph>
+            ) : null
+          }
         </Box>
       </CardBody>
 
@@ -54,8 +97,8 @@ function WikiTile({ element, category }) {
 
         {category !== 'book_elements' ?
           <Paragraph size='small'>{category.charAt(0).toUpperCase() + category.slice(1, -1)}</Paragraph>
-          : <Paragraph size='small'>Misc</Paragraph>}
-
+          : <Paragraph size='small'>Misc</Paragraph>
+        }
         <WikiDeleteButton element={element} category={category} />
       </CardFooter>
     </Card>
